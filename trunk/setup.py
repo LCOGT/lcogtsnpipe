0b21cfc9279dcf23038276fe999b421bd3d8ef0b
Griffin Hosseinzadeh <g.hosseinzadeh90@gmail.com>
2016-05-02 21:57:24 +0000

    fix previous commit for installing scripts

--- trunk/setup.py
+++ trunk/setup.py
@@ -1,60 +1,23 @@
 from distutils.core import setup
-from distutils.command.install import INSTALL_SCHEMES
-from os import sys, path
-import os,shutil,re
+from os import path
 from glob import glob
-for scheme in INSTALL_SCHEMES.values():
-    scheme['data'] = scheme['purelib']
-
-
-from imp import find_module
-try: find_module('numpy')
-except: sys.exit('### Error: python module numpy not found')
-    
-try: find_module('astropy')
-except: sys.exit('### Error: python module pyfits not found')
-
-#try: find_module('pyraf')
-#except: sys.exit('### Error: python module pyraf not found')
-
-try: find_module('matplotlib')
-except: sys.exit('### Error: python module matplotlib not found')
-
-#try: find_module('MySQLdb')
-#except: sys.exit('### Error: python module MySQLdb not found')
-
-
-verstr = "unknown"
-try:
-    parentdir=os.getcwd()+'/'
-    verstrline = open(parentdir+'/src/lsc/_version.py', "rt").read()
-except EnvironmentError:
-    pass # Okay, there is no version file.
-else:
-    VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
-    mo = re.search(VSRE, verstrline, re.M)
-    if mo:
-        verstr = mo.group(1)
-    else:
-        raise RuntimeError("unable to find version in "+parentdir+"+src/floyds/_version.py")
-
 
 setup(
     name='lsc',
-    version=verstr,#'0.1.3',
+    version='1.0.0',
     author='S. Valenti',
     author_email='svalenti@lcogt.net',
-    scripts=['bin/*.py'],
+    scripts=glob(path.join('bin', '*.py')),
     url='lcogt.net',
     license='LICENSE.txt', 
     description='lsc is a package to reduce PHTOMETRIC SN data',
     long_description=open('README.txt').read(),
-    requires=['numpy','astropy','matplotlib','MySQLdb'],
+    install_requires=['numpy','astropy','matplotlib','MySQLdb', 'pyraf'],
     packages=['lsc'],
-    package_dir={'':'src'},
-    package_data = {'lsc' : ["standard/astrometry/*cat","standard/*txt","standard/stdlist/*txt",
-                             "standard/cat/*dat","standard/cat/*cat",
-                             "standard/cat/sloan/*cat","standard/cat/landolt/*cat","standard/cat/apass/*cat",
-                             "standard/cat/sloanprime/*cat","standard/cat/sloannatural/*cat","standard/cat/landoltnatural/*cat",
-                             "standard/sex/*"]}
+    package_dir={'': 'src'},
+    package_data={'lsc': ["standard/astrometry/*cat","standard/*txt","standard/stdlist/*txt",
+                         "standard/cat/*dat","standard/cat/*cat",
+                         "standard/cat/sloan/*cat","standard/cat/landolt/*cat","standard/cat/apass/*cat",
+                         "standard/cat/sloanprime/*cat","standard/cat/sloannatural/*cat","standard/cat/landoltnatural/*cat",
+                         "standard/sex/*"]}
 )

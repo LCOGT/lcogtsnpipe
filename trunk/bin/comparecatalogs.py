c0c0ec56e589f1e785fd657c57f479ebcbfdbb10
Griffin Hosseinzadeh <g.hosseinzadeh90@gmail.com>
2016-05-04 21:36:22 +0000

    git pull before reinstalling pipeline

--- trunk/bin/comparecatalogs.py
+++ trunk/bin/comparecatalogs.py
@@ -50,4 +50,5 @@ for system in args.field:
         lsc.mysqldef.query(['update targets set ' + system + '_cat="" where ' + system + '_cat is NULL'], lsc.conn) # change NULL to '' if not in field
 
 os.chdir(gitdir)
+os.system('git pull')
 os.system('python setup.py install')

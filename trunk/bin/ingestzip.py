1db1fa28c1f2cb090f72f26989218b26c4d22d25
Azalee Bostroem <abostroem@gmail.com>
2020-04-27 13:46:43 -0700

    adding script to ingest zip files and fits file from a directory

---
+++ trunk/bin/ingestzip.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+import lsc
+import sys
+import os
+import re
+import string
+import glob
+import subprocess
+import zipfile
+from optparse import OptionParser
+_dir = os.environ['LCOSNDIR']
+
+def unpack_zip(_zipfile):
+    #_targetid = re.sub('.zip','',string.split(_tarfile,'_')[-1])
+    my_zip = zipfile.open(_zipfile)
+    imglist = my_tar.namelist()
+    my_zip.extractall(_dir)
+    my_zip.close()
+    return imglist
+
+def ingest_files(imglist,force=False):
+    if force:       
+        lsc.mysqldef.ingestredu(imglist, 'yes') # ingest new data into photlco
+    else:
+        lsc.mysqldef.ingestredu(imglist, 'no') # ingest new data into photlco
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Downloads data from SNEX')
+    parser.add_argument("-f", "--file")
+    parser.add_argument("-G", "--force-db", action="store_true", help="reingest files even if they already exist")
+    args = parser.parse_args()
+    if args.file:
+        if 'zip' in args.file:
+            imglist = unpack_zip(args.file)
+        else:
+            imglist_fz = glob.glob(os.path.join(args.file, '*.fz'))
+            if len(imglist_fz)>0:
+                for ifile in imglist_fz:
+                    subprocess.Popen("echo 'funpack {}'".format(ifile), shell=True)
+                    process = subprocess.Popen("funpack {}".format(ifile), shell=True)
+                    process.wait()
+            imglist = glob.glob(os.path.join(args.file, '*.fits'))
+        ingest_files(imglist, force=args.force_db)
+    else:
+        print("you must specify a tar or zip file or directory with the -f or --file")
+    
+        

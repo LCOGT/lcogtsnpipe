e45746afc6ebd4b9be9088568450a1e5a9b9b55c
Azalee Bostroem <abostroem@gmail.com>
2020-10-22 10:39:34 -0700

    modifying logic so that when PSF is X, checkpsf stage doesn't try to run on image

--- trunk/src/lsc/myloopdef.py
+++ trunk/src/lsc/myloopdef.py
@@ -467,11 +467,8 @@ def checkstage(img, stage, database='photlco'):
             status = 1  # mag should be replaced with 'cat'
         else:
             status = 2
-    elif stage == 'checkpsf' and status >= -1 and ggg[0]['wcs'] == 0:
-        if ggg[0]['psf'] == 'X':
-            status = 1
-        else:
-            status = 2
+    elif stage == 'checkpsf' and status >= 0 and ggg[0]['psf'] != 'X' and ggg[0]['wcs'] == 0:
+        status = 1
     elif stage == 'checkmag' and status >= 0 and ggg[0]['psf'] != 'X' and ggg[0]['wcs'] == 0:
         if ggg[0]['psfmag'] == 9999:
             status = 1

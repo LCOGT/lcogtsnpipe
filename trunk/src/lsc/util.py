52325201ff827a43031fe1b45e0f9e6af7ee9716
Azalee Bostroem <abostroem@gmail.com>
2024-02-23 23:41:15 +0000

    condensing instrument definitions

--- trunk/src/lsc/util.py
+++ trunk/src/lsc/util.py
@@ -137,7 +137,7 @@ def readkey3(hdr,keyword):
        _instrume=hdr.get('INSTRUME').lower()
     except: 
        _instrume='none'
-    if 'kb' in _instrume: # SBIG
+    if ('kb' in _instrume) or ('sq' in instrume): # SBIG or QHY
         useful_keys = {'object'    : 'OBJECT',\
                            'date-obs'  : 'DATE-OBS',\
                            'ut'        : 'DATE-OBS',\
@@ -162,31 +162,6 @@ def readkey3(hdr,keyword):
                            'propid'      : 'PROPID',\
                            'userid'      : 'USERID',\
                            'telescop'  : 'TELESCOP'}
-    elif 'sq' in _instrume: # CMOS
-        useful_keys = {'object'    : 'OBJECT',\
-                           'date-obs'  : 'DATE-OBS',\
-                           'ut'        : 'DATE-OBS',\
-                           'date-night': 'DAY-OBS',\
-                           'RA'        : 'RA',\
-                           'DEC'       : 'DEC',\
-                           'CAT-RA'    : 'CAT-RA',\
-                           'CAT-DEC'   : 'CAT-DEC',\
-                           'datamin'   :  -100.0,\
-                           'datamax'   : 'SATURATE',\
-                           'observer'  : 'OBSERVER',\
-                           'exptime'   : 'EXPTIME',\
-                           'wcserr'    : 'WCSERR',\
-                           'instrume'  : 'INSTRUME',\
-                           'JD'        : 'MJD-OBS',\
-                           'mjd'        : 'MJD-OBS',\
-                           'filter'    : 'FILTER',\
-                           'gain'      : 'GAIN',\
-                           'ron'       : 'RDNOISE',\
-                           'airmass'   : 'AIRMASS',\
-                           'type'      : 'OBSTYPE',\
-                           'propid'      : 'PROPID',\
-                           'userid'      : 'USERID',\
-                           'telescop'  : 'TELESCOP'} 
     elif 'fl' in _instrume or 'fa' in _instrume: # sinistro
         useful_keys = {'object'    : 'OBJECT',\
                            'date-obs'  : 'DATE-OBS',\

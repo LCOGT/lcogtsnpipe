2a6056d9104c7969ae8d1fbcbc0bd27a63f62b16
jrfarah <jrfarah1999@gmail.com>
2024-06-14 16:28:35 -0700

    setup and imports

--- trunk/bin/automated_subtraction.py
+++ trunk/bin/automated_subtraction.py
@@ -1,1 +1,31 @@
-# placeholder
+#!/usr/bin/env python
+
+
+"""
+====================================
+
+* **Filename**:         run_subtraction.py 
+* **Author**:              Joseph Farah 
+* **Description**:       Automates subtraction on a schedule.
+
+====================================
+
+**Notes**
+*  Observation created on target with ID at MJD. Cronjob runs. 
+*  Two things to check: 
+*    - does template exist for object in filter?
+*    - has observation in filter been conducted after MJD?
+* If yes: get filter, mjd,  filetag. run subtraction with that info
+
+
+Steps:
+- 
+
+
+"""
+
+#------------- IMPORTS -------------#
+import lsc
+import datetime
+from subprocess import call
+from mysql.connector import connect

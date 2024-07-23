cddb8b85b948ac3b1812875df54deb3c9ac04372
jrfarah <jrfarah1999@gmail.com>
2024-07-23 10:02:25 -0700

    changed requires to install_requires

--- trunk/setup.py
+++ trunk/setup.py
@@ -13,7 +13,7 @@ setup(
     license='LICENSE.txt', 
     description='lsc is a package to reduce PHTOMETRIC SN data',
     long_description=open('README.txt').read(),
-    requires=['numpy','astropy','matplotlib','MySQLdb', 'pyraf', 'reproject', 'mysql-connector-python-rf'],
+    install_requires=['numpy','astropy','matplotlib','MySQLdb', 'pyraf', 'reproject', 'mysql-connector-python-rf'],
     packages=['lsc'],
     package_dir={'': 'src'},
     package_data={'lsc': ["standard/astrometry/*cat","standard/*txt","standard/stdlist/*txt",

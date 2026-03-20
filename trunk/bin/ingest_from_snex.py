885211966c1e6d995e52d66be1ff16555ca6ce6d
Griffin Hosseinzadeh <1976665+griffin-h@users.noreply.github.com>
2026-03-20 11:21:58 -0700

    initial commit of ingest_from_snex.py

---
+++ trunk/bin/ingest_from_snex.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+import lsc
+from LCOGTingest import *
+from argparse import ArgumentParser
+import logging
+
+logger = logging.getLogger()
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Ingest data from the Supernova Exchange')
+    parser.add_argument("frames", nargs='+', help="images to ingest")
+    parser.add_argument("-F", "--force", action="store_true", help="reingest files even if they already exist")
+    args = parser.parse_args()
+
+    print(f'Ingesting {len(args.frames)} frames')
+
+    fullpaths = [os.path.abspath(frame) for frame in args.frames]
+    for frame in fullpaths:
+        dbdict = db_ingest(*os.path.split(frame), args.force)
+    lsc.mysqldef.ingestredu(fullpaths, force='yes' if args.force else 'no')  # ingest new data into photlco

cdbe8fdcfcfb0996f6ad3089b81e63c94c746d4e
Sai Praneth Raju K. <sai.praneth10@gmail.com>
2026-04-14 00:30:39 -0700

    Fix relative imports in __init__.py for Python 3
    - Convert implicit relative imports to explicit (from .module import *)
    - Required since Python 3 removed implicit relative imports

--- trunk/src/lsc/__init__.py
+++ trunk/src/lsc/__init__.py
@@ -1,19 +1,19 @@
-from mysqldef import *
-from util import *
-from lscastrodef import *
-from lscabsphotdef import *
-from lscsnoopy import *
-from sqlcl import *
-from sites import *
-from externaldata import *
-from myloopdef import *
-from cosmics import *
-from lscpsfdef import *
-from banzaicat import *
+from .mysqldef import *
+from .util import *
+from .lscastrodef import *
+from .lscabsphotdef import *
+from .lscsnoopy import *
+from .sqlcl import *
+from .sites import *
+from .externaldata import *
+from .myloopdef import *
+from .cosmics import *
+from .lscpsfdef import *
+from .banzaicat import *
 
 __version__ = "unknown"
 try:
-    from _version import __version__
+    from ._version import __version__
 except ImportError:
     # We're running in a tree that doesn't have a _version.py, so we don't know what our version is.
     pass
